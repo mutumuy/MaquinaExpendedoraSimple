e1cb7914402cf254ca66e0661627a654fde504e9
Jose Maria <jmcr2500@gmail.com>
2019-10-17 13:27:36 +0200

    Ahora el precio y el destino se pasan como parametro

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -16,12 +16,12 @@ public class MaquinaExpendedoraSimple {
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple() {
-        precioBillete = 12;
+    public MaquinaExpendedoraSimple(int precioBillete, String destino) {
+        this.precioBillete = precioBillete;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
         estacionOrigen = "Leon";
-        estacionDestino = "Bembibre";
+        estacionDestino = destino;
     }
 
     /**
